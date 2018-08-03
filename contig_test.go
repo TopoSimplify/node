1100e0fadf8beb4971853dbf61d2bc7f4822c62c
Titus T <titus.intdxdt@gmail.com>
2018-08-02 21:36:52 -0300

    test

--- contig_test.go
+++ contig_test.go
@@ -1,10 +1,9 @@
 package node
 
 import (
+	"time"
 	"testing"
 	"github.com/franela/goblin"
-	"fmt"
-	"time"
 )
 
 func TestIsContiguous(t *testing.T) {
@@ -15,12 +14,6 @@ func TestIsContiguous(t *testing.T) {
 			//checks if score is valid at threshold of constrained dp
 			var coords = linearCoords("LINESTRING ( 960 840, 980 840, 980 880, 1020 900, 1080 880, 1120 860, 1160 800, 1160 760, 1140 700, 1080 700, 1040 720, 1060 760, 1120 800, 1080 840, 1020 820, 940 760 )")
 			var hulls = createHulls([][]int{{0, 2}, {2, 6}, {6, 8}, {8, 10}, {10, 12}, {12, coords.Len() - 1}}, coords)
-			fmt.Println(hulls[0].Geom.WKT())
-			fmt.Println(hulls[1].Geom.WKT())
-			var intersects =  hulls[0].Geom.Intersection(hulls[1].Geom)
-			for _, o := range intersects{
-				fmt.Println(o.WKT())
-			}
 
 			inters, contig, n := IsContiguous(&hulls[0], &hulls[1])
 			g.Assert(inters).IsTrue()
